2f524e897e5c9c5667d4e4e0f4fa67be1d55a5aa
unknown <viana.fabricio1@gmail.com>
2022-01-09 18:49:15 -0300

    First commit

---
+++ satelites.py
@@ -0,0 +1,5 @@
+satelites = [
+    {"name": "kenobi", "coordenadasX" : -500 , "coordenadasY" : -200},
+    {"name": "skywalker", "coordenadasX" : 100, "coordenadasY" : -100},
+    {"name": "sato", "coordenadasX" : 500, "coordenadasY" : 100}
+]
